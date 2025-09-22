f97f774829247a077c5bafe5c96cbff4e171c778
Dat Nguyen <thanhdng@Dats-MacBook-Pro.local>
2025-09-22 12:55:01 +0200

    test set up

---
+++ 1.test_sample.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=sarek1
+#SBATCH --output=_sarek1-%j.out
+#SBATCH --account=nn9114k
+#SBATCH --time=100:00:00
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=16G
+
+module purge
+module load Nextflow/24.04.2
+module load Apptainer
+
+mkdir -p logs results_sarek_test2
+
+# Centralized Nextflow Apptainer cache (shared filesystem)
+export APPTAINER_CACHEDIR=/cluster/projects/nn9114k/datngu/apptainer
+export NXF_APPTAINER_CACHEDIR=/cluster/projects/nn9114k/datngu/apptainer
+
+# Run the local clone of nf-core/sarek with own samplesheet and selected tools
+nextflow run main.nf -profile saga \
+  --input /cluster/projects/nn9114k/datngu/projects/variant_calling/toy_data/samplesheet.csv \
+  --genome GATK.GRCh38 \
+  --tools deepvariant,manta,cnvkit \
+  --outdir results_sarek_test2 \
