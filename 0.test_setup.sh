f514e8e396aab0a6ca8679cfef73317273fa7544
Dat Nguyen <thanhdng@Dats-MacBook-Pro.local>
2025-09-22 12:13:08 +0200

    test set up

--- 0.test_setup.sh
+++ 0.test_setup.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=sarek-test
 #SBATCH --output=logs/sarek-test-%j.out
 #SBATCH --account=nn9114k
-#SBATCH --time=02:00:00
+#SBATCH --time=100:00:00
 #SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=16G
 
@@ -12,7 +12,7 @@ module load Nextflow/24.04.2
 
 mkdir -p logs results_sarek_test
 
-export APPTAINER_CACHEDIR=/cluster/projects/nn9114k/datngu/singularity
+export NXF_APPTAINER_CACHEDIR=/cluster/projects/nn9114k/datngu/singularity
 
 
 nextflow run main.nf -profile saga,test \
