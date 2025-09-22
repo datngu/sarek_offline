1d25f878bc5e39c892de494be3735500a92b29d4
Dat Nguyen <thanhdng@Dats-MacBook-Pro.local>
2025-09-22 11:46:54 +0200

    test set up

---
+++ 0.test_setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=sarek-test
+#SBATCH --output=logs/sarek-test-%j.out
+#SBATCH --account=nn9114k
+#SBATCH --time=02:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=8G
+
+module purge
+module load Nextflow/24.04.2
+module load Apptainer
+mkdir -p logs results_sarek_test
+export APPTAINER_CACHEDIR=/cluster/work/users/$USER/apptainer/cache
+mkdir -p "$APPTAINER_CACHEDIR"
+
+nextflow run nf-core/sarek -r 3.5.1 -profile saga,test \
+  --outdir results_sarek_test \
+  -with-report report.html -with-trace trace.txt \
+  -with-timeline timeline.html -with-dag dag.png
