4555118e36d3a5593849d00a9f2d0aa3737300ee
Dat Nguyen <thanhdng@Dats-MacBook-Pro.local>
2025-09-22 11:47:31 +0200

    test set up

--- 0.test_setup.sh
+++ 0.test_setup.sh
@@ -10,7 +10,7 @@ module purge
 module load Nextflow/24.04.2
 module load Apptainer
 mkdir -p logs results_sarek_test
-export APPTAINER_CACHEDIR=/cluster/work/users/$USER/apptainer/cache
+export APPTAINER_CACHEDIR=/cluster/projects/nn9114k/datngu/singularity
 mkdir -p "$APPTAINER_CACHEDIR"
 
 nextflow run nf-core/sarek -r 3.5.1 -profile saga,test \
