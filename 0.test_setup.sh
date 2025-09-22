eb75a602cbf8aed37992d5eef41999134af042a5
Dat Nguyen <thanhdng@Dats-MacBook-Pro.local>
2025-09-22 11:57:02 +0200

    test set up

--- 0.test_setup.sh
+++ 0.test_setup.sh
@@ -15,7 +15,7 @@ mkdir -p logs results_sarek_test
 export APPTAINER_CACHEDIR=/cluster/projects/nn9114k/datngu/singularity
 
 
-nextflow run main.nf -r 3.5.1 -profile saga,test \
+nextflow run main.nf -profile saga,test \
   --outdir results_sarek_test \
   -with-report report.html -with-trace trace.txt \
   -with-timeline timeline.html -with-dag dag.png
