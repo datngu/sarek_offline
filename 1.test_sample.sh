f06a49f7505882aa199dc594bbf3af51d17870b8
Dat Nguyen <thanhdng@Dats-MacBook-Pro.local>
2025-10-04 00:05:27 +0200

    rm deepvariant

--- 1.test_sample.sh
+++ 1.test_sample.sh
@@ -39,6 +39,6 @@ nextflow run main.nf -profile saga \
   -c offline_hg38.config \
   --input /cluster/projects/nn9114k/datngu/projects/variant_calling/toy_data/samplesheet.csv \
   --genome HG38_OFFLINE \
-  --tools deepvariant,manta,cnvkit,haplotypecaller \
+  --tools manta,cnvkit,haplotypecaller \
   --outdir results_sarek_offline_test \
   -resume
